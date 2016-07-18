0257afc2fac7011f1246180ae4f4ea5c6de3bc03
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-07-18 22:25:11 +0700

    Updates

--- README.md
+++ README.md
@@ -3,8 +3,196 @@ Color Picker
 
 > A simple color picker plugin in pure JavaScript, for modern browsers.
 
-[View Demo](https://rawgit.com/tovic/color-picker/master/color-picker.html)
+[![View Demo](https://cloud.githubusercontent.com/assets/1669261/16919759/246196ec-4d35-11e6-8d12-153aa969384e.png)](https://rawgit.com/tovic/color-picker/master/color-picker.html "View Demo")
 
-On the way…
+~~~ .html
+<!DOCTYPE html>
+<html dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+    <link href="color-picker.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <input type="text">
+    <script src="color-picker.min.js"></script>
+    <script>
+    var picker = new CP(document.querySelector('input[type="text"]'));
+    picker.on("drag", function(color) {
+        picker.target.value = '#' + color;
+    });
+    </script>
+  </body>
+</html>
+~~~
 
 Has support for touch events. Touchy… touchy…
+
+Hooks
+-----
+
+The available hooks:
+
+ - `create`
+ - `destroy`
+ - `enter`
+ - `exit`
+ - `fit`
+ - `start`
+ - `start:h`
+ - `start:sv`
+ - `drag`
+ - `drag:h`
+ - `drag:sv`
+ - `stop`
+
+### Add a Hook
+
+Add a `drag` hook.
+
+~~~ .javascript
+picker.on("drag", function(color) {
+    console.log(color);
+});
+~~~
+
+### Add a Hook with ID
+
+Add a `drag` hook with ID of `test-id`.
+
+~~~ .javascript
+picker.on("drag", function(color) {
+    console.log(color);
+}, 'test-id');
+~~~
+
+### Remove a Hook
+
+Remove all `drag` hooks.
+
+~~~ .javascript
+picker.off("drag");
+~~~
+
+### Remove a Hook by ID
+
+Remove a `drag` hook with ID of `test-id`.
+
+~~~ .javascript
+picker.off("drag", 'test-id');
+~~~
+
+### Trigger a Hook with Custom Value
+
+Trigger all `drag` hooks with pre–defined color value as `ffa500`.
+
+~~~ .javascript
+picker.trigger("drag", 'ffa500');
+~~~
+
+Trigger a `drag` hook with ID of `test-id` and with pre–defined color value as `ffa500`.
+
+~~~ .javascript
+picker.trigger("drag", 'ffa500', 'test-id');
+~~~
+
+Data
+----
+
+### Get the Target Element
+
+~~~ .javascript
+var target = picker.target;
+~~~
+
+### Get the Picker Element
+
+~~~ .javascript
+var picker = picker.picker;
+~~~
+
+### Get Hidden Color Data on the Target Element
+
+~~~ .javascript
+console.log(picker.set());
+~~~
+
+### Set Hidden Color Data on the Target Element
+
+~~~ .javascript
+picker.set([0, 1, 1]); // HSV color value, range from `0` to `1` for each
+~~~
+
+~~~ .javascript
+picker.set('rgb(255, 0, 0)'); // as color string
+~~~
+
+Color Converter
+---------------
+
+### HSV to RGB
+
+~~~ .javascript
+console.log(picker.HSV2RGB([360, 100, 100]));
+~~~
+
+### HSV to HEX
+
+~~~ .javascript
+console.log(picker.HSV2HEX([360, 100, 100]));
+~~~
+
+### RGB to HSV
+
+~~~ .javascript
+console.log(picker.RGB2HSV([255, 255, 255]));
+~~~
+
+### RGB to HEX
+
+~~~ .javascript
+console.log(picker.RGB2HEX([255, 255, 255]));
+~~~
+
+### HEX to HSV
+
+~~~ .javascript
+console.log(picker.HEX2HSV('ffffff'));
+~~~
+
+### HEX to RGB
+
+~~~ .javascript
+console.log(picker.HEX2RGB('ffffff'));
+~~~
+
+Events
+------
+
+### Show
+
+~~~ .javascript
+picker.enter(); // show the color picker
+~~~
+
+### Hide
+
+~~~ .javascript
+picker.exit(); // hide the color picker
+~~~
+
+Want to Add More Features?
+--------------------------
+
+My purpose in making this plugin is to provide a JavaScript color picker solution as simple as possible with the most minimal features without requiring any dependency on JavaScript library such as jQuery or Prototype.
+
+If you want to add new features, you can use the available hooks to make your own improvements without having to touch the plugin core. Here are some examples:
+
+ - [Multiple Instances](https://rawgit.com/tovic/color-picker/master/color-picker.picker.html)
+ - [Pre–Defined Value](https://rawgit.com/tovic/color-picker/master/color-picker.value-set.html)
+ - [Pre–Defined Color](https://rawgit.com/tovic/color-picker/master/color-picker.picker-set.html)
+ - [Convert HEX Color Value](https://rawgit.com/tovic/color-picker/master/color-picker.value-convert.html)
+ - [Show and Hide with Buttons](https://rawgit.com/tovic/color-picker/master/color-picker.state.html)
+ - [Add Close Button](https://rawgit.com/tovic/color-picker/master/color-picker.close.html)
+ - [Create and Destroy Method](https://rawgit.com/tovic/color-picker/master/color-picker.create-destroy.html)
+ - etc… (coming soon!)
---
+++ color-picker.js
@@ -0,0 +1,442 @@
+/*!
+ * ==========================================================
+ *  COLOR PICKER PLUGIN 1.0.0
+ * ==========================================================
+ * Author: Taufik Nurrohman <http://latitudu.com>
+ * Licensed under the MIT license.
+ * ----------------------------------------------------------
+ */
+
+var CP = function(target) {
+
+    var w = window,
+        d = document,
+        r = this,
+        noop = function() {},
+        hooks = {},
+        picker = d.createElement('div');
+
+    function isset(x) {
+        return typeof x !== "undefined";
+    }
+
+    // [h, s, v] ... 0 <= h, s, v <= 1
+    function HSV2RGB(a) {
+        var h = +a[0],
+            s = +a[1],
+            v = +a[2],
+            r, g, b, i, f, p, q, t;
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        if (isNaN(i)) i = 0;
+        if (isNaN(q)) q = 0;
+        if (isNaN(t)) t = 0;
+        switch (i % 6) {
+            case 0:
+                r = v, g = t, b = p;
+                break;
+            case 1:
+                r = q, g = v, b = p;
+                break;
+            case 2:
+                r = p, g = v, b = t;
+                break;
+            case 3:
+                r = p, g = q, b = v;
+                break;
+            case 4:
+                r = t, g = p, b = v;
+                break;
+            case 5:
+                r = v, g = p, b = q;
+                break;
+        }
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    }
+    function HSV2HEX(a) {
+        return RGB2HEX(HSV2RGB(a));
+    }
+
+    // [r, g, b] ... 0 <= r, g, b <= 255
+    function RGB2HSV(a) {
+        var r = +a[0],
+            g = +a[1],
+            b = +a[2],
+            max = Math.max(r, g, b),
+            min = Math.min(r, g, b),
+            d = max - min,
+            h, s = (max === 0 ? 0 : d / max),
+            v = max / 255;
+        switch (max) {
+            case min:
+                h = 0;
+                break;
+            case r:
+                h = (g - b) + d * (g < b ? 6 : 0);
+                h /= 6 * d;
+                break;
+            case g:
+                h = (b - r) + d * 2;
+                h /= 6 * d;
+                break;
+            case b:
+                h = (r - g) + d * 4;
+                h /= 6 * d;
+                break;
+        }
+        return [h, s, v];
+    }
+    function RGB2HEX(a) {
+        var s = +a[2] | (+a[1] << 8) | (+a[0] << 16);
+        s = '000000' + s.toString(16);
+        return s.slice(-6);
+    }
+
+    // rrggbb or rgb
+    function HEX2HSV(s) {
+        return RGB2HSV(HEX2RGB(s));
+    }
+    function HEX2RGB(s) {
+        if (s.length === 3) {
+            s = s.replace(/./g, '$&$&');
+        }
+        return [parseInt(s[0] + s[1], 16), parseInt(s[2] + s[3], 16), parseInt(s[4] + s[5], 16)];
+    }
+
+    // convert range from `0` to `360` and `0` to `100` in color into range from `0` to `1`
+    function _2HSV_pri(a) {
+        return [1 * (+a[0] / 360), 1 - (+a[1] / 100), 1 - (+a[2] / 100)];
+    }
+
+    // convert range from `0` to `1` in color into `0` to `360` and `0` to `100`
+    function _2HSV_pub(a) {
+        return [Math.round(+a[0] * 360), Math.round(+a[1] * 100), Math.round(+a[2] * 100)];
+    }
+
+    // *
+    function _2HSV(x) {
+        var rgb = /\s*rgb\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)\s*$/i.exec(x),
+            hsv = /\s*hsv\s*\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*\)\s*$/i.exec(x),
+            // hsl = /\s*hsl\s*\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*\)\s*$/i.exec(x), TODO: add support for HSL color!
+            hex = x[0] === '#' && x.match(/^#([\da-f]{3}|[\da-f]{6})$/);
+        if (hex) {
+            return HEX2HSV(x.slice(1));
+        // } else if (hsl) {
+        //     return ...
+        } else if (hsv) {
+            return _2HSV_pri([+hsv[1], +hsv[2], +hsv[3]]);
+        } else if (rgb) {
+            return RGB2HSV([+rgb[1], +rgb[2], +rgb[3]]);
+        }
+        return [0, 1, 1]; // default is red
+    }
+
+    // add event
+    function on(ev, el, fn) {
+        return el.addEventListener(ev, fn, false);
+    }
+
+    // remove event
+    function off(ev, el, fn) {
+        return el.removeEventListener(ev, fn);
+    }
+
+    // get mouse/finger coordinate
+    function point(el, e) {
+      var x = !!e.touches ? e.touches[0].pageX : e.pageX,
+          y = !!e.touches ? e.touches[0].pageY : e.pageY,
+          left = el.offsetLeft,
+          top = el.offsetTop;
+      while (el = el.offsetParent) {
+          left += el.offsetLeft;
+          top += el.offsetTop;
+          el = el.offsetParent;
+      }
+      return {
+          x: x - left,
+          y: y - top
+      };
+    }
+
+    // get position
+    function offset(el) {
+        return {
+            l: el.offsetLeft,
+            t: el.offsetTop
+        };
+    }
+
+    // get dimension
+    function size(el) {
+        return {
+            w: el.offsetWidth,
+            h: el.offsetHeight
+        };
+    }
+
+    // get color data
+    function get_data(el, a) {
+        var s = el.getAttribute('data-color');
+        s = s ? s.split(',') : [];
+        if (!isset(a)) a = false;
+        return s.length === 3 ? [+s[0], +s[1], +s[2]] : a;
+    }
+
+    // set color data
+    function set_data(el, a) {
+        if (a === false) {
+            return el.removeAttribute('data-color');
+        }
+        return el.setAttribute('data-color', a.join(','));
+    }
+
+    // add hook
+    function add(ev, fn, id) {
+        if (!isset(hooks[ev])) hooks[ev] = {};
+        if (!isset(ev)) return hooks;
+        if (!isset(fn)) return hooks[ev];
+        if (!isset(id)) return hooks[ev][Object.keys(hooks[ev]).length] = fn;
+        hooks[ev][id] = fn;
+    }
+
+    // remove hook
+    function remove(ev, id) {
+        if (!isset(ev)) return hooks = {};
+        if (!isset(id)) return hooks[ev] = {};
+        delete hooks[ev][id];
+    }
+
+    // trigger hook
+    function trigger(ev, v, id) {
+        if (!isset(hooks[ev])) return;
+        if (!isset(id)) {
+            for (var i in hooks[ev]) {
+                hooks[ev][i](v);
+            }
+        } else {
+            if (isset(hooks[ev][id])) {
+                hooks[ev][id](v);
+            }
+        }
+    }
+
+    // initialize data ...
+    if (target.value && target.value.length) {
+        set_data(target, _2HSV(target.value));
+    } else if (!target.value) {
+        var data = get_data(target, target.getAttribute('data-color'));
+        if (data) set_data(target, _2HSV(data));
+    }
+
+    // generate color picker pane ...
+    picker.className = 'color-picker';
+    picker.innerHTML = '<div class="color-picker-control"><span class="color-picker-h"><i></i></span><span class="color-picker-sv"><i></i></span></div>';
+    var c = picker.firstChild.children,
+        HSV = get_data(target, [0, 1, 1]), // default is red
+        H = c[0],
+        SV = c[1],
+        H_point = H.firstChild,
+        SV_point = SV.firstChild,
+        drag_H = false,
+        drag_SV = false,
+        left = 0,
+        top = 0,
+        P_W = 0,
+        P_H = 0,
+        set, exit;
+
+    // fit to window
+    function fit() {
+        left = offset(target).l;
+        top = offset(target).t + size(target).h;
+        if (left + P_W > w.innerWidth) {
+            left = w.innerWidth - P_W;
+        }
+        if (top + P_H > w.innerHeight) {
+            top = w.innerHeight - P_H;
+        }
+        picker.style.left = left + 'px';
+        picker.style.top = top + 'px';
+        trigger("fit", r);
+    };
+
+    // create
+    function create(first) {
+        if (!first) d.body.appendChild(picker);
+        P_W = size(picker).w;
+        P_H = size(picker).h;
+        var H_H = size(H).h,
+            SV_W = size(SV).w,
+            SV_H = size(SV).h,
+            H_point_H = size(H_point).h,
+            SV_point_W = size(SV_point).w,
+            SV_point_H = size(SV_point).h, v;
+        if (first) {
+            picker.style.left = '-9999px';
+            picker.style.top = '-9999px';
+            on("resize", w, fit);
+            function click(e) {
+                create();
+                trigger("click", r);
+                e.stopPropagation();
+            } on("click", target, click);
+            r.create = function() {
+                create(1);
+                trigger("create", r);
+            };
+            r.destroy = function() {
+                off("click", target, click);
+                set_data(target, false);
+                exit();
+                trigger("destroy", r);
+            };
+        } else {
+            fit();
+            trigger("enter", r);
+        }
+        set = function() {
+            HSV = get_data(target, HSV);
+            color();
+            H_point.style.top = ((H_H * +HSV[0]) - (H_point_H / 2)) + 'px';
+            SV_point.style.right = (SV_W - (SV_point_W / 2) - (SV_W * +HSV[1])) + 'px';
+            SV_point.style.top = (SV_H - (SV_point_H / 2) - (SV_H * +HSV[2])) + 'px';
+        };
+        exit = function() {
+            if (picker.parentNode) d.body.removeChild(picker);
+            off("touchmove", d, move);
+            off("mousemove", d, move);
+            off("touchend", d, stop);
+            off("mouseup", d, stop);
+            off("touchdown", d, exit);
+            off("click", d, exit);
+            trigger("exit", r);
+        };
+        function color(e) {
+            var a = HSV2RGB(HSV),
+                b = HSV2RGB([HSV[0], 1, 1]);
+            SV.style.backgroundColor = 'rgb(' + b.join(',') + ')';
+            set_data(target, HSV);
+            if (e) e.preventDefault();
+        } set();
+        function do_H(e) {
+            var o = point(H, e), y = o.y;
+            if (y < 0) {
+                y = 0;
+            } else if (y > H_H) {
+                y = H_H;
+            }
+            HSV[0] = 1 - ((H_H - y) / H_H);
+            H_point.style.top = (y - (H_point_H / 2)) + 'px';
+            color(e);
+        }
+        function do_SV(e) {
+            var o = point(SV, e),
+                x = o.x,
+                y = o.y;
+            if (x < 0) {
+                x = 0;
+            } else if (x > SV_W) {
+                x = SV_W;
+            }
+            if (y < 0) {
+                y = 0;
+            } else if (y > SV_H) {
+                y = SV_H;
+            }
+            HSV[1] = 1 - ((SV_W - x) / SV_W);
+            HSV[2] = (SV_H - y) / SV_H;
+            SV_point.style.right = (SV_W - x - (SV_point_W / 2)) + 'px';
+            SV_point.style.top = (y - (SV_point_H / 2)) + 'px';
+            color(e);
+        }
+        function move(e) {
+            if (drag_H) {
+                do_H(e);
+                v = HSV2HEX(HSV);
+                trigger("drag:h", v);
+                trigger("drag", v);
+            }
+            if (drag_SV) {
+                do_SV(e);
+                v = HSV2HEX(HSV);
+                trigger("drag:sv", v);
+                trigger("drag", v);
+            }
+        }
+        function stop() {
+            drag_H = false;
+            drag_SV = false;
+            if (!first) trigger("stop", HSV2HEX(HSV)); // TODO: add `stop:h` and `stop:sv` hook
+        }
+        function down_H(e) {
+            drag_H = true;
+            do_H(e);
+            trigger("start:h", r);
+            trigger("start", r);
+        }
+        function down_SV(e) {
+            drag_SV = true;
+            do_SV(e);
+            trigger("start:sv", r);
+            trigger("start", r);
+        }
+        on("touchstart", H, down_H);
+        on("mousedown", H, down_H);
+        on("touchstart", SV, down_SV);
+        on("mousedown", SV, down_SV);
+        on("touchmove", d, move);
+        on("mousemove", d, move);
+        on("touchend", d, stop);
+        on("mouseup", d, stop);
+        on("touchdown", d, exit);
+        on("click", d, exit);
+    } create(1);
+
+    w.setTimeout(function() {
+        trigger("create", HSV2HEX(HSV));
+    }, .1);
+
+    // register to global ...
+    r.target = target;
+    r.picker = picker;
+    r.on = add;
+    r.off = remove;
+    r.trigger = trigger;
+    r.fit = fit;
+    r.set = function(a) {
+        if (typeof a === "string") {
+            a = _2HSV(a);
+        }
+        if (!isset(a)) return get_data(target);
+        set_data(target, a), set();
+    };
+    r.HSV2RGB = function(a) {
+        return HSV2RGB(_2HSV_pri(a));
+    };
+    r._HSV2RGB = HSV2RGB;
+    r.HSV2HEX = function(a) {
+        return HSV2HEX(_2HSV_pri(a));
+    };
+    r._HSV2HEX = HSV2HEX;
+    r.RGB2HSV = function(a) {
+        return _2HSV_pub(RGB2HSV(a));
+    };
+    r._RGB2HSV = RGB2HSV;
+    r.RGB2HEX = RGB2HEX;
+    r.HEX2HSV = function(s) {
+        return _2HSV_pub(HEX2HSV(s));
+    };
+    r._HEX2HSV = HEX2HSV;
+    r.HEX2RGB = HEX2RGB;
+    r._2HSV = _2HSV;
+    r.hooks = hooks;
+    r.enter = create;
+    r.exit = exit;
+
+    // return the global data
+    return r;
+
+};
