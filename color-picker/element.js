65832509f9cffb6ce88925acbd2f82331ccb7562
Taufik Nurrohman <hi@taufik-nurrohman.com>
2020-11-02 10:28:09 +0700

    Update

--- color-picker/element.js
+++ color-picker/element.js
@@ -1,4 +1,4 @@
-(function(win, doc) {
+((win, doc) => {
 
     class HTMLColorPickerElement extends HTMLElement {
 
@@ -17,15 +17,14 @@
                 input = doc.createElement('input'),
                 inputName = this.getAttribute('name'),
                 inputValue = this.getAttribute('value');
-            source.style.cssText = 'display:inline-block;vertical-align:middle;width:2em;height:2em;cursor:pointer;';
             input.type = 'hidden';
             if (inputName) {
                 input.name = inputName;
             }
             if (inputValue) {
                 input.value = inputValue;
+                source.dataset.color = inputValue;
                 source.style.backgroundColor = inputValue;
-                source.setAttribute('data-color', inputValue);
             }
             root.appendChild(source);
             root.host.parentNode.insertBefore(input, root.host);
@@ -33,19 +32,24 @@
             const picker = new CP(source);
             picker.on('change', function(r, g, b, a) {
                 let color = this.color(r, g, b, a);
+                source.dataset.color = color;
                 source.style.backgroundColor = color;
-                source.setAttribute('data-color', color);
                 input.value = color;
             });
+            picker.self.style.marginTop = '-1px';
+            source.style.border = '1px solid #000';
+            source.style.cursor = 'pointer';
+            source.style.display = 'inline-block';
+            source.style.height = '2em';
+            source.style.verticalAlign = 'middle';
+            source.style.width = '2em';
             // TODO: Remove this event listener
-            source.addEventListener('click', function() {
-                picker.enter();
-            }, false);
+            source.addEventListener('click', () => picker.enter());
         }
 
     }
 
-    // Define the new element
+    // Define custom element
     win.customElements.define('color-picker', HTMLColorPickerElement);
 
-})(this, this.document);
+})(window, document);
